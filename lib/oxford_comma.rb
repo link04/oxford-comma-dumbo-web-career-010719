b78e80a7e2ce926d404c57155b2032c1ff5ee12a
Maximo Bautista <max.bautista97@gmail.com>
2018-12-25 15:11:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,27 @@
 def oxford_comma(array)
-
+  if array.size == 1 
+    array.join()
+  elsif array.size == 2 
+    array.join(" and ")
+  elsif array.size == 3
+    string = ""
+    array.each do |fruit|
+      if array.index(fruit) < 2
+        string << "#{fruit}, "
+      else
+        string << "and #{fruit}"
+      end
+    end
+   string
+  elsif array.size > 3
+    string = ""
+    array.each do |fruit|
+      if array.index(fruit) < array.size - 1
+        string << "#{fruit}, "
+      else
+        string << "and #{fruit}"
+      end
+    end
+      string
+  end
 end
