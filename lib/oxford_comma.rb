18f1a731d3d81f3967588e57fa1d726bfa3725a2
Maximo Bautista <max.bautista97@gmail.com>
2018-12-25 14:56:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,8 @@ def oxford_comma(array)
   elsif array.size == 2 
     array.join(" and ")
   elsif array.size == 3
-    array.join(", " + " and ")
+    string = ""
+    array.each do |fruit|
+      string << fruit
   end
 end
